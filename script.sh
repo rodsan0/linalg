f41ef997adda009fb36bc4b0372fca68a5b58418
rodsan0 <rodsan0@users.noreply.github.com>
2020-11-02 20:09:29 -0500

    Fix flattening script

--- script.sh
+++ script.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-C_FILES=$(find linalg/ -type f \( -name '*.c' ! -name 'main.c' ! -name 'tests.c' \))
-H_FILES=$(find linalg/ -type f \( -name '*.h' ! -name 'tests.h' \))
+C_FILES=$(find -type f \( -name '*.c' ! -name 'main.c' ! -name 'tests.c' \))
+H_FILES=$(find -type f \( -name '*.h' ! -name 'tests.h' \))
 
 
 echo $C_FILES
 echo $H_FILES
 
-echo "#pragma once" > linalg.h
+mkdir -p single-include
+echo "#pragma once" > single-include/linalg.hpp
+
 
 for file in $H_FILES $C_FILES; do
-    cat $file | sed 's/#pragma once//g' | sed 's/#include ".*"//g' >> single_include/linalg.hpp
+    cat $file | sed 's/#pragma once//g' | sed 's/#include ".*"//g' >> single-include/linalg.hpp
 done
 
 
