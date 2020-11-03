2181475483eb268d37839c2f70df9149a4d1e339
rodsan0 <rodsan0@users.noreply.github.com>
2020-11-02 19:57:36 -0500

    Add script to flatten library.

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+C_FILES=$(find linalg/ -type f \( -name '*.c' ! -name 'main.c' ! -name 'tests.c' \))
+H_FILES=$(find linalg/ -type f \( -name '*.h' ! -name 'tests.h' \))
+
+
+echo $C_FILES
+echo $H_FILES
+
+echo "#pragma once" > linalg.h
+
+for file in $H_FILES $C_FILES; do
+    cat $file | sed 's/#pragma once//g' | sed 's/#include ".*"//g' >> linalg.h
+done
+
+
