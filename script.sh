5d9508f1118f410c23d868885ebba8e7e845196b
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-11-11 23:40:13 -0500

    Wrap single-header code in hola namespace

--- script.sh
+++ script.sh
@@ -9,10 +9,19 @@ echo $H_FILES
 
 mkdir -p single-include
 echo "#pragma once" > single-include/linalg.hpp
+echo "namespace hola {" >> single-include/linalg.hpp
 
 
 for file in $H_FILES $C_FILES; do
     cat $file | sed 's/#pragma once//g' | sed 's/#include ".*"//g' >> single-include/linalg.hpp
 done
 
+echo "} // namespace hola" >> single-include/linalg.hpp
 
+sed -i 's/#define OWNS_MEMORY(object) (((struct linalg_obj\*) object)->owns_memory)/#define OWNS_MEMORY(object) (((struct hola::linalg_obj\*) object)->owns_memory)/g' single-include/linalg.hpp
+
+sed -i 's/#define MEMORY_OWNER(object) (((struct linalg_obj\*) object)->memory_owner)/#define MEMORY_OWNER(object) (((struct hola::linalg_obj*) object)->memory_owner)/g' single-include/linalg.hpp
+
+sed -i 's/#define REF_COUNT(object) (((struct linalg_obj\*) object)->ref_count)/#define REF_COUNT(object) (((struct hola::linalg_obj*) object)->ref_count)/g' single-include/linalg.hpp
+
+sed -i 's/#define DATA(object) (((struct linalg_obj\*) object)->data)/#define DATA(object) (((struct hola::linalg_obj*) object)->data)/g' single-include/linalg.hpp
